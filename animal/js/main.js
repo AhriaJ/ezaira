ad1aab46ad24a661c56576f4f571caca0f5e3669
AhriaJ <bnm7412@gmail.com>
2024-07-04 13:19:07 +0900

    .

--- animal/js/main.js
+++ animal/js/main.js
@@ -150,24 +150,33 @@ $(document).ready(function(){
 
     //family의 swiper
     const family_swiper = new Swiper('.family .swiper', { /* 팝업을 감싼는 요소의 class명 */
-	slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
-	spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
-	breakpoints: {
-		768: {    /* 768px 이상일때 적용 */
-			slidesPerView: 'auto',
-			spaceBetween: 18,
-		},
-        1024: {   /* 1024px 이상일때 적용 */
-			slidesPerView: 'auto',
-			spaceBetween: 24,
-		},
-	},
-	centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
-	loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
-	navigation: {
-		nextEl: '.family .btn_wrap .next',
-		prevEl: '.family .btn_wrap .prev',
-	},
-});
+        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            768: {    /* 768px 이상일때 적용 */
+                slidesPerView: 'auto',
+                spaceBetween: 18,
+            },
+            1024: {   /* 1024px 이상일때 적용 */
+                slidesPerView: 'auto',
+                spaceBetween: 24,
+            },
+        },
+        centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        navigation: {
+            nextEl: '.family .btn_wrap .next',
+            prevEl: '.family .btn_wrap .prev',
+        },
+    });
+    /* family_site (관련사이트) 열고 닫기
+        footer .family_site button.open를 클릭하면 family_site에 open 클래스
+        footer .family_site button.close 클릭하면 family_site에 open 클래스 삭제 */
+    $('footer .family_site button.open').on('click', function(){
+        $('footer .family_site').addClass('open')
+    })
+    $('footer .family_site button.close').on('click', function(){
+        $('footer .family_site').removeClass('open')
+    })
 
 })//document
