6abee247b98f9176e2ac0159ef541898aeec2fde
AhriaJ <bnm7412@gmail.com>
2024-07-18 13:48:24 +0900

    뉴스와 서비스

--- Kumho/js/main.js
+++ Kumho/js/main.js
@@ -51,4 +51,21 @@ $(document).ready(function(){
         $('.biz .list ul li').removeClass('on')
         $('.biz .list ul li').removeClass('off')
     })
-})
+
+    const news_swiper = new Swiper('.news .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            768: {    /* 1280px 이상일때 적용 */
+                slidesPerView: 'auto',
+                spaceBetween: 24,
+            },
+        },
+        scrollbar: {
+            el: ".news .swiper-scrollbar",
+            hide: false,
+            draggable: true,
+          },
+    });
+
+})//끝
