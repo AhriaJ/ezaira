872b736399bd344c39a4ee0cefe98cb6cdc716cb
AhriaJ <bnm7412@gmail.com>
2024-08-23 13:17:48 +0900

    생명의 숲 비쥬얼과 사이드 메뉴

--- forest/js/main.js
+++ forest/js/main.js
@@ -18,11 +18,39 @@ $(document).ready(function(){
 		afterLoad: function(origin, destination, direction, trigger){
 			if((destination.index == 1)||(destination.index == 3)||(destination.index == 4)){
 			    $('header').addClass('dark')
+				$('.fp_nav').addClass('dark')
+				$('.quick').addClass('dark')
 			}else{
                 $('header').removeClass('dark')
+				$('.fp_nav').removeClass('dark')
+				$('.quick').removeClass('dark')
             }
+			console.log(destination.index)
+			$('.fp_nav ul li').removeClass('active')
+			$('.fp_nav ul li').eq(destination.index).addClass('active')
 		},
 
 		responsiveWidth: 1024 /* fullpage를 적용시키지 않을 모바일 사이즈 */
+	});//fullpage
+
+	const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
+
+		autoplay: {  /* 팝업 자동 실행 */
+			delay: 5000,
+			disableOnInteraction: true,
+		},
+	
+		loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+	
+		pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+			el: '.visual .paging', /* 해당 요소의 class명 */
+			clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+			renderBullet: function (index, className) {   /* paging에 특정 코드 넣기 */
+				return '<div class="item '+ className +'"><span class="txt'+(index + 1)+'"></span></div>';
+			},
+		},
+		
 	});
+	swiper.autoplay.stop();  /* 일시정지 기능 */
+	swiper.autoplay.start();  /* 재생 기능 */
 })
