3678187ec416a723a7dd440764a3b32415923b2b
AhriaJ <bnm7412@gmail.com>
2024-08-23 10:09:18 +0900

    생명의 숲 왼쪽 메뉴

--- forest/js/main.js
+++ forest/js/main.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
     const myFullpage = new fullpage('#fullpage', {  /* html에서 페이지 전체를 감싸는 요소 */
 
-		navigation: true, /* 오른쪽에 각 페이지의 paging */
+		navigation: false, /* 오른쪽에 각 페이지의 paging */
 		navigationPosition: 'left', /* 위치 */
 		navigationTooltips: ['Main', '나무심기', '숲 활동', '활동이야기'], /* 툴팁 */
 		showActiveTooltip: true, /* 현재 활성화된 페이지의 툴팁에 특정 클래스 주기 */
+
+		anchors: ['visual', 'tree', 'forest', 'story'], /* href="#link1" 이렇게 코딩하면 해당 링크명으로 이동 */
 
 		autoScrolling:true, /* 한페이지씩 스크롤 */
 		scrollHorizontally: true,
