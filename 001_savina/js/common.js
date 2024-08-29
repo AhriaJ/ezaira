fa893fc016746d06b4c0a3566a7992e47ac050a5
AhriaJ <bnm7412@gmail.com>
2024-08-29 18:00:53 +0900

    포폴01 사비나

---
+++ 001_savina/js/common.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    /* 
+        header .gnb .gnb_wrap ul.depth1 > li
+        pc 일때 메뉴에 마우스오버하면 
+        1. header에 menu_over 클래스 추가
+        2. 마우스를 오버한 li에 on클래스 추가 > pc에서만 구현
+    */
+
+    $('header .gnb .gnb_wrap ul.depth1 > li').on('mouseenter focusin', function(){
+        if
+    })
+    
+})
